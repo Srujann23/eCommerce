1efe6c1375afdb364a1ef3195ddc3320b4f44211
Srujan <srujankachhadia23@gmail.com>
2024-09-24 10:07:21 +0530

    commit3

---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+  return (
+    <div className='text-center'>
+        <p className="text-2x1 font-medium text-gray-800">Subscribe Now & Get 20% off.</p>
+    </div>
+  )
+} 
+export default NewsLetterBox
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
-export const content = [
-  './src/**/*.{js,jsx,ts,tsx}',
-  './public/index.html',
-];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
