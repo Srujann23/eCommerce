4796bf10c545988a154eeca543027c70a2d7ca73
Srujan <srujankachhadia23@gmail.com>
2024-10-05 00:09:07 +0530

    ok

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay } from '../controllers/orderController.js'
+// import { placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay } from '../controllers/orderController.js'
+import { placeOrder, allOrders, userOrders, updateStatus } from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 const orderRouter = express.Router()
@@ -11,14 +12,14 @@ orderRouter.post('/status',adminAuth,updateStatus);
 
 //payment features
 orderRouter.post('/place',authUser,placeOrder)
-orderRouter.post('/stripe',authUser,placeOrderStripe)
-orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
+// orderRouter.post('/stripe',authUser,placeOrderStripe)
+// orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
 
 //user features
 orderRouter.post('/userorders',authUser,userOrders)
 
 //verify payment
-orderRouter.post('/verifyStripe',authUser,verifyStripe)
-orderRouter.post('/verifyRazorpay',authUser,verifyRazorpay)
+// orderRouter.post('/verifyStripe',authUser,verifyStripe)
+// orderRouter.post('/verifyRazorpay',authUser,verifyRazorpay)
 
 export default orderRouter
