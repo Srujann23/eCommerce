7c32aa6f9efbbdb5ef576e0b27ecdf90f122e1cb
Srujan <srujankachhadia23@gmail.com>
2024-10-05 12:30:14 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader(
       "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Origin",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.setHeader(
