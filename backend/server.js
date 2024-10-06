2c40c6a915a3af55a8d9fe395b3826a499b948a4
Srujan <srujankachhadia23@gmail.com>
2024-10-06 11:47:24 +0530

    ok1

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,11 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
 
 app.use(morgan('dev'))
 
