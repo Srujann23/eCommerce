18951a4bdc29f560e82066f6c82507a5caefe224
Srujan <srujankachhadia23@gmail.com>
2024-10-06 11:57:32 +0530

    ok1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import morgan from 'morgan';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
@@ -9,40 +9,31 @@ import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
-//App Config
+// App Config
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
-//midddelwares
+// CORS middleware
 app.use(cors({
-    origin: '*',
+    origin: '*', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204);  // Handle the preflight request
-    }
-    next();
-});
 
+app.use(morgan('dev'));
+app.use(express.json());
 
-app.use(morgan('dev'))
-app.use(express.json())
-
-//api eps
-app.use('/api/user', userRouter)
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
+// API endpoints
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello ')
-})
+    res.send('Hello');
+});
 
-app.listen(port, () => console.log("Server Running on PORT: " + port))
+// Server listening
+app.listen(port, () => console.log("Server Running on PORT: " + port));
